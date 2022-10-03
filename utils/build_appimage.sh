49f565712c53edd1cfbc4664c39e175385fdc943
quyykk <quyykk@protonmail.com>
2022-10-03 18:18:40 +0200

    Fix builds.

--- utils/build_appimage.sh
+++ utils/build_appimage.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ -z "$1" ]; then
+  echo "Please provide the path to the build directory!"
+  exit 1
+fi
+
 # Builds Endless Sky and packages it as AppImage
 # Control the output filename with the OUTPUT environment variable
 # You may have to set the ARCH environment variable to e.g. x86_64.
@@ -9,7 +14,7 @@ set -e
 cp resources/icons/icon_512x512.png endless-sky.png
 
 # Install
-DESTDIR=AppDir cmake --install build/linux --prefix /usr
+DESTDIR=AppDir cmake --install "$1" --prefix /usr
 
 # Inside an AppImage, the executable is a link called "AppRun" at the root of AppDir/.
 # Keeping the data files next to the executable is perfectly valid, so we just move them to AppDir/ to avoid errors.
@@ -17,7 +22,7 @@ mv AppDir/usr/share/games/endless-sky/* AppDir/
 
 # Now build the actual AppImage
 curl -sSL https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -o linuxdeploy && chmod +x linuxdeploy
-./linuxdeploy --appdir AppDir -e build/linux/Release/endless-sky -d resources/endless-sky.desktop -i endless-sky.png --output appimage
+./linuxdeploy --appdir AppDir -e "$1/endless-sky" -d resources/endless-sky.desktop -i endless-sky.png --output appimage
 
 # Clean up
 rm -rf AppDir linuxdeploy endless-sky.png
