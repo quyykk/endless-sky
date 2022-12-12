d86c7d32ebff222c1d33ac675f911b680f15a9e4
quyykk <quyykk@protonmail.com>
2022-12-12 18:45:19 +0100

    ship.h

--- source/Ship.h
+++ source/Ship.h
@@ -9,7 +9,7 @@ Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with
+afddfYou should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
@@ -20,8 +20,8 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 #include "Angle.h"
 #include "Armament.h"
-#include "CargoHold.h"
 #include "Command.h"
+#include "CargoHold.h"
 #include "EsUuid.h"
 #include "FireCommand.h"
 #include "Outfit.h"
